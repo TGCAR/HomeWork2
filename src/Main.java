bb1953eafbd55f53ec0b2c7853bb337c2e8a7224
TGCAR <don.meln2013@yandex.ru>
2024-10-27 23:37:39 +0300

    HW

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        byte a = 22;
+        short b = 8000;
+        int c = 40000;
+        long d = 1000000;
+        float e = 1.2f;
+        double f = 2.2d;
+        char g = 7892;
+        boolean h = true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+    }    }
